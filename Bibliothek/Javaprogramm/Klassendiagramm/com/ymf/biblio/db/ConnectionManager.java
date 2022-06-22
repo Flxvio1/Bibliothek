2f64792965689c8315507b3cbfed4f5bd8fa1a37
mannaluca02 <mannaluca02@gmail.com>
2022-06-22 14:10:47 +0200

    added comments beautify sql statement code easy switch from DB

--- Bibliothek/Javaprogramm/Klassendiagramm/com/ymf/biblio/db/ConnectionManager.java
+++ Bibliothek/Javaprogramm/Klassendiagramm/com/ymf/biblio/db/ConnectionManager.java
@@ -21,24 +21,12 @@ public class ConnectionManager {
 	public static java.sql.Connection getConnection() throws SQLException, ClassNotFoundException {
 		String serverAuswahl = "MariaDB"; // Auswahl welche DB verwendet werden soll
 		Connection con;
-		if(serverAuswahl.equals("MariaDB")){
-			// Für MariaDB
-			System.out.println("Use MariaDB");
-			Class.forName("org.mariadb.jdbc.Driver");
-			con = DriverManager.getConnection(
-					"jdbc:mysql://192.168.40.6:3306/bibliothek?user=lufla&password=password");
-		} else if(serverAuswahl.equals("PostgreSql")){
 			// Für PostgreSql
 			System.out.println("Use PostgreSql");
 			Class.forName("org.postgresql.jdbc.Driver");
 			con = DriverManager.getConnection(
 					"jdbc:postgresql://192.168.40.63:5432/bibliothek?user=lufla&password=password");
-		} else{
-			// Wenn die Eingabe Falsch ist, wird automatisch MariaDB verwendet:
-			System.out.println("Use MariaDB");
-			con = DriverManager.getConnection(
-					"jdbc:mysql://192.168.40.6:3306/bibliothek?user=lufla&password=password");
-		}
+
   		//Gibt con zurück, damit verbinung von anderen Klassen möglich ist
 		return con;
 
