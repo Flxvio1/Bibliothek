3ccde20198080bb1b0433ea472634f3e3e4c1bc0
Flxvio1 <flavio.schaffer@bbzbl-it.ch>
2022-06-23 08:35:57 +0200

    Methode neuerKunde is fixed

--- Bibliothek/Javaprogramm/Klassendiagramm/com/ymf/biblio/db/KundenVerwaltungDao.java
+++ Bibliothek/Javaprogramm/Klassendiagramm/com/ymf/biblio/db/KundenVerwaltungDao.java
@@ -137,19 +137,19 @@ public class KundenVerwaltungDao implements KundeDao {
             }
 
             //SQL Befehl um den Kunden in der Kundentabelle hinzuzufügen.
-            insertSql = "insert into kunde (name, vorname, email ,adresse_idadresse) " +
-                  "values ( '" + nachname + "'," +
-                   ", '" + vorname + "'," +
-                    ", '" + email + "'," +
-                    ", '" + field + "');";
+           // insertSql = "insert into kunde (name, vorname, email ,adresse_idadresse) " +
+                //  "values ( '" + nachname + "'," +
+                //   ", '" + vorname + "'," +
+                 //   ", '" + email + "'," +
+                 //   ", '" + field + "');";
             
-           /* insertSql = "insert into kunde (name,vorname,email,adresse_idadresse) values (?,?,?,?);";
+            insertSql = "insert into kunde (name,vorname,email,adresse_idadresse) values (?,?,?,?);";
             stmnt = con.prepareStatement(insertSql);
             stmnt.setString(1, nachname);
             stmnt.setString(2, vorname);
             stmnt.setString(3, email);
-            stmnt.setInt(4, Integer.getInteger(field));
-            stmnt.execute();*/
+            stmnt.setInt(4, 42);
+            stmnt.execute();
 
 
         } catch (Exception e) {
